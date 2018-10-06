3f225c5dd61ee50d637184e8e5fd978a630bd7a6
Dimas Saputra <dimas.maulanads@gmail.com>
2018-10-06 20:53:40 +0700

    Initial commit - Create Login Activity

---
+++ app/src/main/java/id/riotfallen/firstprac/LoginActivity.kt
@@ -0,0 +1,27 @@
+package id.riotfallen.firstprac
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val defaultEmail = "dimas.maulanads@gmail.com"
+        val defaultPasswword = "qwerty123"
+
+        buttonLogin.setOnClickListener {
+            val isEmailValid: Boolean = defaultEmail == editTextEmail.text.toString()
+            val isPasswordValid: Boolean = defaultPasswword == editTextPassword.text.toString()
+            val isAuthValid = isEmailValid && isPasswordValid
+
+
+            val massage: String = if(isAuthValid) "Success" else "false"
+            Toast.makeText(this@LoginActivity, "Login $massage", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
